221e6803e7baca44c672b726a8d74a234d701c9b
bud aminof <budaminof@gmail.com>
2017-03-08 12:14:33 -0700

    following stephen tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,42 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+// an array with strings- names of the libraries which we want to include in
+// this separate vendor file.
+const VENDOR_LIBS = [
+  'react', 'lodash', 'redux', 'react-redux', 'react-dom', 'react-input-range',
+  'redux-form', 'redux-thunk', 'faker'
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    //[name] will be replaced with the entry bundle file name
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 };
